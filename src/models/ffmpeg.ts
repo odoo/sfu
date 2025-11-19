51d792bddf3d22e9bcfec18b1fe1c28f3d61c0b6
ThanhDodeurOdoo <tso@odoo.com>
2025-11-19 09:00:29 +0100

    [wip] recorder/task Implement dynamic FFMPEG management for audio recording with explicit task events and standardized parameter handling.

--- src/models/ffmpeg.ts
+++ src/models/ffmpeg.ts
@@ -1,8 +1,12 @@
 import { EventEmitter } from "node:events";
 
+let currentId = 0;
+
 export class FFMPEG extends EventEmitter {
+    id: number;
     constructor() {
         super();
+        this.id = currentId++ % 999999;
     }
 
     async kill() {}
